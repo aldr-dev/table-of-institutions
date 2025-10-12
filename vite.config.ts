72c7517985e0b38269ac7256e6f3db5f871a1827
Gordus Alexander <aldrmiller@gmail.com>
2025-10-12 17:21:17 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  // base: '/table-of-institutions/',
+  base: '/table-of-institutions/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
